ebfa6d2e68846a8668d5788bb6b082e8b3bd1a3b
imeepos <1037483576@qq.com>
2019-07-25 22:07:49 +0800

    feat: 🎸 feat

--- apps/apis/lib/app.ts
+++ apps/apis/lib/app.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { EntitiesModule } from '@luna/entities';
 @Module({
     imports: [
+        EntitiesModule.forRoot({
+            magnus: {
+                port: 4000
+            }
+        }),
         GraphQLModule.forRoot({
             typePaths: [join(__dirname, 'assets/apis/magnus.graphql')]
         })
---
+++ libs/entities/lib/index.ts
@@ -0,0 +1,62 @@
+import { Module, DynamicModule, Inject } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { bootstrap as magnusBootstrap } from '@notadd/magnus-core';
+import { Controller } from './entities/controller';
+import { createConnection } from 'typeorm';
+const metadata = require('./assets/entities/magnus.metadata.json');
+const server = require('./assets/entities/magnus.server.json');
+const entity = require('./assets/entities/magnus.entity.json');
+import {
+    Member
+} from './entities/index';
+import { Magnus } from './runner';
+export * from './runner';
+import { ErrorsInterceptor } from './errorsInterceptor';
+require('dotenv').config();
+
+@Module({
+    imports: [],
+    providers: [Magnus, { provide: APP_INTERCEPTOR, useClass: ErrorsInterceptor }],
+    exports: [Magnus]
+})
+export class EntitiesModule {
+    static environment: any;
+    constructor(@Inject(`ENVIRONMENT`) public environment: any) { }
+    static forRoot(environment: any = {}): DynamicModule {
+        environment = environment || {};
+        environment.db = environment.db || {};
+        return {
+            module: EntitiesModule,
+            providers: [
+                {
+                    provide: `ENVIRONMENT`,
+                    useValue: environment
+                }
+            ]
+        };
+    }
+    async onModuleInit() {
+        const environment = this.environment;
+        await createConnection({
+            type: environment.db.type || 'postgres',
+            host: environment.db.host || '10.0.1.2',
+            port: environment.db.port || 5432,
+            database: environment.db.database || 'magnus',
+            username: environment.db.username || 'default',
+            password: environment.db.password || 'secret',
+            entities: [
+                Member
+            ],
+            synchronize: environment.db.synchronize || true
+        });
+        return await magnusBootstrap(
+            metadata,
+            server,
+            {
+                Controller
+            },
+            entity,
+            this.environment.magnus.port
+        );
+    }
+}
