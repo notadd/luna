ad554b9f5c2a1f6fbb2609aec49256e17cec2173
zhixilu <321420745@qq.com>
2019-07-26 14:08:24 +0800

    feat: 🎸 update app.resolver

--- apps/apis/lib/app.resolver.ts
+++ apps/apis/lib/app.resolver.ts
@@ -1,18 +1,25 @@
 import { Resolver, Query } from "@nestjs/graphql";
-import { FindAllTixianLogResult } from "./magnus/type";
+import { FindAllTixianLogResult, FindAllTixianLogData } from "./magnus/type";
 import { Magnus } from "@luna/entities";
 
 @Resolver()
 export class AppResolver {
-    constructor(public readonly magnus: Magnus){}
+  constructor(public readonly magnus: Magnus) {}
 
   @Query()
   hello(): string {
     return `hello`;
   }
 
-//   @Query()
-//   async findAllTixianLog(): Promise<FindAllTixianLogResult> {
-//     await this.magnus.query.findTixianLog();
-//   }
+  @Query()
+  async findAllTixianLog(): Promise<FindAllTixianLogResult> {
+    const res = await this.magnus.query.findTixianLog<FindAllTixianLogData[]>(
+      {},
+      `{id, count, createDate, status, member{id, nikename}}`
+    );
+    return {
+      code: 200,
+      data: res
+    };
+  }
 }
